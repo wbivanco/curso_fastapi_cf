f6452b22ae4dedebcbb621b7eb9e413e46a81db0
Walter Bivanco <walterbivanco@gmail.com>
2024-10-03 11:51:45 -0300

    Modulo 5 - Clase 12

--- project/schemas.py
+++ project/schemas.py
@@ -1,6 +1,19 @@
-from pydantic import BaseModel
-from pydantic import validator
+from pydantic import BaseModel, validator
+from pydantic.utils import GetterDict
 
+from typing import Any
+
+from peewee import ModelSelect
+
+
+class PeeweeGetterDict(GetterDict):
+    def get(self, key: Any, default: Any = None):
+        res = getattr(self._obj, key, default)
+        if isinstance(res, ModelSelect):
+            return list(res)
+        
+        return res
+    
 
 class UserRequestModel(BaseModel):
     username: str
@@ -17,3 +30,7 @@ class UserRequestModel(BaseModel):
 class UserResponseModel(BaseModel):
     id: int
     username: str
+
+    class Config:
+        orm_mode = True
+        getter_dict = PeeweeGetterDict
