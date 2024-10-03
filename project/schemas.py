ab27dee745b334d10b91c65142c549ac19980ed1
Walter Bivanco <walterbivanco@gmail.com>
2024-10-03 09:43:06 -0300

    Modulo 5 clase 7

--- project/schemas.py
+++ project/schemas.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel
+from pydantic import validator
 
 
 class UserBaseModel(BaseModel):
     username: str
     password: str
+
+    @validator('username')
+    def username_validator(cls, usarname):
+        if len(usarname) < 3 and len(usarname) > 50:
+            raise ValueError('La longitud debe estar entre 3 y 50 caracteres.')
+        
+        return usarname
