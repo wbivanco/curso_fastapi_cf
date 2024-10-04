7e849780616b145c4c587d4ba24624e593107246
Walter Bivanco <walterbivanco@gmail.com>
2024-10-04 09:07:21 -0300

    Modulo 5 - Clase 19

--- project/main.py
+++ project/main.py
@@ -96,3 +96,14 @@ async def update_review(review_id: int, review_request: ReviewRequestPutModel):
     user_review.save()
 
     return user_review
+
+@app.delete('/reviews/{review_id}', response_model=ReviewResponseModel)
+async def delete_review(review_id: int):
+    user_review = UserReview.select().where(UserReview.id == review_id).first()
+
+    if user_review is None:
+        raise HTTPException(404, 'La reseña no existe.')
+    
+    user_review.delete_instance()
+
+    return user_review
