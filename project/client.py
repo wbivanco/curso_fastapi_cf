c0ab1f614535736a667aed55c81bbe5fd8309329
Walter Bivanco <wb@Mac-WB.local>
2024-10-04 16:59:14 -0300

    Modulo 6 - clase 3

--- project/client.py
+++ project/client.py
@@ -2,8 +2,9 @@ import requests
 
 URL = 'http://127.0.0.1:8000/api/v1/reviews'
 HEADERS = {'accept': 'application/json'}
+QUERYSET = {'page': 1, 'limit': 2}
 
-response = requests.get(URL, headers=HEADERS)
+response = requests.get(URL, headers=HEADERS, params=QUERYSET)
 
 if response.status_code == 200:
     print('Petición realizada de forma exitosa!')
